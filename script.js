f473b3559fd74ad48e7205e2cfd34e7f18f76811
Mohammad Abid <68217349+abidzmohammad@users.noreply.github.com>
2022-08-27 00:11:42 +0600

    functionality done

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overly = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function(){
+   modal.classList.remove('hidden');
+   overly.classList.remove('hidden');
+}
+const closeModal = function(){
+   modal.classList.add('hidden');
+   overly.classList.add('hidden');
+}
+
+for(let i = 0; i < btnOpenModal.length; i++){
+   btnOpenModal[i].addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', closeModal);
+document.addEventListener('keypress', function(){
+   modal.classList.add('hidden');
+   overly.classList.add('hidden');
+})
